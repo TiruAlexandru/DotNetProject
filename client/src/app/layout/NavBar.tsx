4be1eb455fad072383ce31231fc4c6641afc71d6
TiruAlexandru <alexandru6565@gmail.com>
2025-05-24 10:58:37 +0300

    End of section 9

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,14 +1,19 @@
 import { Group } from "@mui/icons-material";
-import { AppBar, Box, Toolbar, Typography, Container, MenuItem } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography, Container, MenuItem, LinearProgress } from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLink from "../shared/components/MenuItemLink";
+import { Observer } from "mobx-react-lite";
+import { useStore } from "../../lib/hooks/UseStore";
 
 export default function NavBar() {
+  const { uiStore } = useStore();
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{
-        backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'
-      }}>
+      <AppBar position="static"
+        sx={{
+          backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)',
+          position: 'relative'
+        }}>
         <Container maxWidth='xl' >
           <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <Box>
@@ -24,12 +29,30 @@ export default function NavBar() {
               <MenuItemLink to='/createActivity'>
                 Create Activity
               </MenuItemLink>
+              <MenuItemLink to='/counter'>
+                Counter
+              </MenuItemLink>
             </Box>
             <MenuItem>
               User menu
             </MenuItem>
           </Toolbar>
         </Container>
+
+        <Observer>
+          {() => uiStore.isLoading ? (
+            <LinearProgress
+              color="secondary"
+              sx={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                height: 4,
+              }}></LinearProgress>
+          ) : null}
+        </Observer>
+
       </AppBar>
     </Box>
   )
